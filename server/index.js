4a3daf10c57c9ef5443557ba2b58e1411f56d374
YourName <your.email@example.com>
2025-04-28 11:48:21 +0700

    Update MongoDB configuration to use Atlas

--- server/index.js
+++ server/index.js
@@ -19,13 +19,25 @@ app.use(express.urlencoded({ extended: true }));
 // Serve static files from the public directory
 app.use(express.static(path.join(__dirname, '../public')));
 
-// Connect to MongoDB
-mongoose.connect(process.env.MONGODB_URI, {
+// MongoDB connection options
+const mongoOptions = {
   useNewUrlParser: true,
-  useUnifiedTopology: true
-})
-.then(() => console.log('Connected to MongoDB'))
-.catch(err => console.error('MongoDB connection error:', err));
+  useUnifiedTopology: true,
+  retryWrites: true,
+  w: 'majority'
+};
+
+// Connect to MongoDB
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb+srv://Rettey:Adhu1447@cluster0.spr2o17.mongodb.net/?retryWrites=true&w=majority';
+
+mongoose.connect(MONGODB_URI, mongoOptions)
+  .then(() => {
+    console.log('Connected to MongoDB Atlas');
+  })
+  .catch(err => {
+    console.error('MongoDB connection error:', err);
+    process.exit(1);
+  });
 
 // Routes
 app.use('/api/auth', authRoutes);
